afca2e07abd9f73333ae455f913c0d0aed1354e3
kitsunai <kitsunai@sanritz.co.jp>
2013-07-31 02:22:39 -0700

    hoge

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 PWD_DIR=`pwd`
-DOT_FILES=$( gitconfig vimrc zshrc vim tmux.conf )
+DOT_FILES=( gitconfig vimrc zshrc vim tmux.conf )
 for file in ${DOT_FILES}
 do
     rm -rf ~/.${file}
