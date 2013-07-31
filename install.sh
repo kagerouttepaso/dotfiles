e0643bf48a3d122cddfb183fc266fe664d47edcc
kitsunai <kitsunai@sanritz.co.jp>
2013-07-30 23:03:26 -0700

    インストールできるようにした

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
-ln .gitconfig ../.gitconfig
-ln .vimrc     ../.vimrc
-ln .zshrc     ../.zshrc
+rm -rf ../.gitconfig
+rm -rf ../.vimrc
+rm -rf ../.zshrc
+rm -rf ../.vim
+
+ln    .gitconfig ../.gitconfig
+ln    .vimrc     ../.vimrc
+ln    .zshrc     ../.zshrc
+ln -s .vim       ../.vim
