9a4ebc1055c369ab895acd6c5564546039900243
kitsunai <kitsunai@sanritz.co.jp>
2013-07-31 00:39:27 -0700

    インストーラーしゅうせい

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
-rm -rf ../.gitconfig
-rm -rf ../.vimrc
-rm -rf ../.zshrc
-rm -rf ../.vim
+rm -rf ~/.gitconfig
+rm -rf ~/.vimrc
+rm -rf ~/.zshrc
+rm -rf ~/.vim
 
-ln    .gitconfig ../.gitconfig
-ln    .vimrc     ../.vimrc
-ln    .zshrc     ../.zshrc
-ln -s .vim       ../.vim
+PWD_DIR=`pwd`
+ln -s ${PWD_DIR}/_gitconfig ~/.gitconfig
+ln -s ${PWD_DIR}/_vimrc     ~/.vimrc
+ln -s ${PWD_DIR}/_zshrc     ~/.zshrc
+ln -s ${PWD_DIR}/_vim       ~/.vim
+
+cd 
 if [ ! -d .vim/bundle/neobundle.vim ] ; then
 git clone https://github.com/Shougo/neobundle.vim.git ./.vim/bundle/neobundle.vim
 fi
