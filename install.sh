985365c447d5f4a91f71f409455b096e901f956b
kitsunai <kitsunai@sanritz.co.jp>
2013-07-31 01:59:13 -0700

    インストーラー修正

--- install.sh
+++ install.sh
@@ -1,14 +1,26 @@
-rm -rf ~/.gitconfig
-rm -rf ~/.vimrc
-rm -rf ~/.zshrc
-rm -rf ~/.vim
-
 PWD_DIR=`pwd`
-ln -s ${PWD_DIR}/_gitconfig ~/.gitconfig
-ln -s ${PWD_DIR}/_vimrc     ~/.vimrc
-ln -s ${PWD_DIR}/_zshrc     ~/.zshrc
-ln -s ${PWD_DIR}/_vim       ~/.vim
+DOT_FILES=(gitconfig vimrc zshrc vim)
+for file in ${DOT_FILES}
+do
+    rm -rf ~/.${file}
+    ln -s ${PWD_DIR}/_${file} ~/.${file}
+done
+
 
 if [ ! -d ~/.vim/bundle/neobundle.vim ] ; then
 git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
 fi
+
+#case "${OSTYPE}" in
+#freebsd*|darwin*)
+#    if [ ! -e ~/bin ];then
+#        mkdir ~/bin
+#    fi
+#    cp bin/tmuxx $HOME/bin/
+#    cp bin/reattach-to-user-namespace $HOME/bin
+#    echo "alias tmux='tmuxx'" >> ~/.bashrc
+#    echo "alias tm='tmuxx'" >> ~/.bashrc
+#    echo "alias tma='tmux attach'" >> ~/.bashrc
+#    echo "alias tml='tmux list-window'" >> ~/.bashrc
+#  ;;
+#esac
