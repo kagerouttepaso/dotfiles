501b211772a52da3ace8a80059bada95251519df
Kitsunai Daisuke <kitsunai@sanritz.co.jp>
2013-08-01 09:30:08 +0900

    hoge

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
-set HOME_DIR=""
-set DOT_DIR=""
+set HOME_DIR=C:\Users\userdir
+set DOT_DIR=C:\cygwin\home\dotdir
 mklink %HOME_DIR%\_vimrc  %DOT_DIR%\_vimrc
-mklink /d \%HOME_DIR%.vim \%DOT_DIR%_vim
+mklink /d %HOME_DIR%\.vim %DOT_DIR%\_vim
