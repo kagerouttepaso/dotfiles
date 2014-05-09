edbe40348b465d80c1b8b14e10283c3addb70d3f
kitsunai <goth.wasawasa@gmail.com>
2014-05-09 16:41:59 +0900

    マルチスレッド化

--- update_submodule.sh
+++ update_submodule.sh
@@ -1,12 +1,12 @@
-#!/bin/bash -x
+#!/bin/bash
 PWD_DIR=`pwd`
 
 SUBMODULES=(./_autojump ./_oh-my-zsh ./_vim/bundle/neobundle.vim)
 for module in ${SUBMODULES[@]}
 do
     cd ${PWD_DIR}/${module}
-    git fetch ; git rebase origin/master
+    sh -c "echo \"update ${module}\";git fetch ; git rebase origin/master" &
 done
-
+wait
 
 
