d4a9122309b799927ac27dbb572458f9de1ace49
kitsunai <kitsunai@sanritz.co.jp>
2014-05-19 18:15:47 +0900

    update bashrc. souce tree対応

--- install.bat
+++ install.bat
@@ -6,6 +6,8 @@ SET FONTREG=%FONG_DIR%\SetInconsolataFont.reg
 mklink %HOME_DIR%\_vimrc  %DOT_DIR%\_vimrc
 mklink %HOME_DIR%\_gvimrc %DOT_DIR%\_gvimrc
 mklink %HOME_DIR%\.gitconfig %DOT_DIR%\_gitconfig
+mklink %HOME_DIR%\.bashrc %DOT_DIR%\_bashrc
+mklink /d %HOME_DIR%\.bash %DOT_DIR%\_bash
 cd %FONG_DIR%
 start ./Inconsolata-Bold.ttf
 start ./Inconsolata-Regular.ttf
