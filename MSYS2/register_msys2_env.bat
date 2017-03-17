838181601a65ae963e45169c73ded3df75029b75
kitsunai <goth.wasawasa@gmail.com>
2017-03-17 17:58:09 +0900

    update msys2 setting

--- MSYS2/register_msys2_env.bat
+++ MSYS2/register_msys2_env.bat
@@ -1,2 +1,8 @@
+rem シンボリック有効(mintty.exeを管理者として実行するよう設定しないと意味がない)
 setx MSYS "winsymlinks:nativestrict"
+rem PATHの設定を引き継ぐ
 setx MSYS2_PATH_TYPE "inherit"
+rem MINGW64を選択
+setx MSYSTEM "MINGW64"
+rem mintty.exeを呼び出した場所をカレントフォルダとする
+setx CHERE_INVOKING "enabled_from_arguments"
