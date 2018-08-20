a16f0110065e27a25c88d76d82b3b0d3da6cf26e
kagerouttepaso <goth.wasawasa@gmail.com>
2018-08-20 15:09:47 +0900

    update

--- install.bat
+++ install.bat
@@ -9,3 +9,8 @@ mklink %HOME_DIR%\_vsvimrc %DOT_DIR%\_vsvimrc
 mklink %HOME_DIR%\.gitconfig %DOT_DIR%\_gitconfig
 mklink %HOME_DIR%\.bashrc %DOT_DIR%\_bashrc
 mklink /d %HOME_DIR%\.bash %DOT_DIR%\_bash
+
+echo cmdópÇÃê›íË
+mklink /H %HOME_DIR%\powerline_core.lua   %DOT_DIR%\_cmder-powerline\powerline_core.lua
+mklink /H %HOME_DIR%\powerline_prompt.lua %DOT_DIR%\_cmder-powerline\powerline_prompt.lua
+mklink /H %HOME_DIR%\powerline_git.lua    %DOT_DIR%\_cmder-powerline\powerline_git.lua
