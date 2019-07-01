e1d3fee3a5414cf49cf006794b4a82dc4c5d45a3
kagerouttepaso <goth.wasawasa@gmail.com>
2019-07-01 15:46:41 +0900

    update

--- windows/xwin.bat
+++ windows/xwin.bat
@@ -1,1 +1,1 @@
-start C:\cygwin64\bin\XWin.exe :0 -clipboard -multiwindow -dpi 100 -listen tcp
+start C:\cygwin64\bin\XWin.exe :0 -fp "C:/Windows/Fonts" -clipboard -multiwindow -dpi 100 -listen tcp -xkbmodel jp106 -xkblayout jp
